655d86a526f08c759b085a876329a7f8eb1b0dff
vic1707 <28602203+vic1707@users.noreply.github.com>
2021-12-13 16:07:26 +0100

    remove spaces

--- .functions.zsh
+++ .functions.zsh
@@ -14,7 +14,7 @@ function ex ()
       *.tar.gz)    tar xzf $1   ;;
       *.tar.xz)    tar xJf $1   ;;
       *.bz2)       bunzip2 $1   ;;
-      *.rar)       unrar x $1     ;;
+      *.rar)       unrar x $1   ;;
       *.gz)        gunzip $1    ;;
       *.tar)       tar xf $1    ;;
       *.tbz2)      tar xjf $1   ;;
