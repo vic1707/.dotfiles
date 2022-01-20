65a84665426f8ca3b48caf59ffd44701b0081748
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-01-20 09:26:49 +0100

    update p10k + install fonts

--- install.zsh
+++ install.zsh
@@ -2,3 +2,10 @@ ln -s "$HOME/.dotfiles/.zshrc" ~/.zshrc
 ln -s "$HOME/.dotfiles/.p10k.zsh" ~/.p10k.zsh
 ln -s "$HOME/.dotfiles/.gitconfig" ~/.gitconfig
 ln -s "$HOME/.dotfiles/.zsh_history" ~/.zsh_history
+
+## fonts
+if [ "$(uname)" = "Darwin" ]; then
+  cp -r $HOME/.dotfiles/.fonts/* "$HOME/Library/Fonts"
+else
+  cp -r $HOME/.dotfiles/.fonts/* "$HOME/.local/share/fonts"
+fi
