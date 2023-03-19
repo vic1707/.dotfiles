785c1a52f04f81b8868066b25eecf8765d3da3a1
vic1707 <dev@vic1707.xyz>
2023-03-19 22:52:57 +0100

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -26,7 +26,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 curl -fsSL https://xmake.io/shget.text | bash
 echo "Xmake, nvm & rust installed"
 
-source $NVM_DIR/nvm.sh && nvm install node
+source .nvm/nvm.sh && nvm install node
 ~/.cargo/bin/cargo install nushell
 /opt/homebrew/bin/brew install nvim
 echo "Additional softwares installed"
