16b4a2500e7c22abe500bfdc2610556f92d1c96c
vic1707 <dev@vic1707.xyz>
2023-03-19 21:34:16 +0100

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -6,8 +6,18 @@ ln -fs "$HOME/.dotfiles/.zsh_history" ~/.zsh_history
 
 ## fonts
 if [ "$(uname)" = "Darwin" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew install zsh-autosuggestions zsh-syntax-highlighting
   cp -r $HOME/.dotfiles/.fonts/* "$HOME/Library/Fonts"
 else
   cp -r $HOME/.dotfiles/.fonts/* "$HOME/.local/share/fonts"
 fi
+
+## install terminal software
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl -fsSL https://xmake.io/shget.text | bash
+# reload $PATH
+source ~/.zshrc
+cargo install nushell
+brew install 
