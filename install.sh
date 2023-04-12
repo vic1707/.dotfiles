2b154df0083a2b8d29ce15a170e2105f60c8f153
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-04-12 12:09:19 +0200

    change font folder

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 ln -fs "~/.dotfiles/.zshrc" ~/.zshrc
 ln -fs "~/.dotfiles/.gitconfig" ~/.gitconfig
+ln -fs "~/.dotfiles/.config/nvim" ~/.config/nvim
 
 echo "Minimal config done"
 
 ## fonts
-cp -r ~/.dotfiles/.fonts/* "$(uname -s | grep -q Darwin && echo "~/Library/Fonts" || echo "~/.local/share/fonts")"
+cp -r ~/.dotfiles/fonts/* "$(uname -s | grep -q Darwin && echo "~/Library/Fonts" || echo "~/.local/share/fonts")"
 echo "Fonts installed"
