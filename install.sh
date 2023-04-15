e5684d1a5b972c2de2249a5b26e0cda0a30e8058
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-04-15 13:37:31 +0200

    feat: requirements checks in `install.sh`

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+function check_command_exists() {
+  if ! command -v "$1" &> /dev/null; then
+    echo "Error: $1 is not installed (command)"
+    exit 1
+  fi
+  if ! type "$1" &> /dev/null; then
+    echo "Error: $1 is not installed (type)"
+    exit 1
+  fi
+  if ! hash "$1" &> /dev/null; then
+    echo "Error: $1 is not installed (hash)"
+    exit 1
+  fi
+}
+
+### Check if commands exists ###
+check_command_exists "zsh"
+check_command_exists "curl"
+check_command_exists "pkg-config"
+echo "Ensure that libssl-dev or eq. is installed"
+read -p "Press enter to continue"
+
+
+# way to source or echo if file doesn't exist (usefull for plugins)
+
 ln -fs "$HOME/.dotfiles/shell/.zshrc" "$HOME/.zshrc"
 ln -fs "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
 mkdir -p "$HOME/.config"
