a31d90eadd48288710bd4ae72576749436eaa109
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-05-02 22:42:39 +0200

    fix: restaure `brew` install macos

--- scripts/mac_softwares.sh
+++ scripts/mac_softwares.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install brew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install brew packages
 declare -a packages=(
