c28f7a03ca6b2fdf0b38c9a1c56f42099954ca11
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-05-05 17:06:40 +0200

    feat: remove out redirections on installs for now

--- scripts/softwares.sh
+++ scripts/softwares.sh
@@ -21,34 +21,34 @@ echo "Installing softwares..."
 ## NVM
 echo "Installing nvm..."
 NVM_VERSION=$(get_latest_github_release "nvm-sh/nvm")
-curl --silent -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh" | bash > /dev/null 2>&1
+curl --silent -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh" | bash 
 echo "Installed nvm"
 
 ## Node
 echo "Installing node..."
-\. ~/.nvm/nvm.sh && nvm install node > /dev/null 2>&1
+\. ~/.nvm/nvm.sh && nvm install node 
 echo "Installed node"
 
 ## Rust
 echo "Installing rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y > /dev/null 2>&1
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 
 echo "Installed rust"
 
 ## Xmake
 echo "Installing xmake..."
-curl -fsSL https://xmake.io/shget.text | bash > /dev/null 2>&1
+curl -fsSL https://xmake.io/shget.text | bash 
 echo "Installed xmake"
 
 ## Starship
 echo "Installing starship..."
-curl -fsSL https://starship.rs/install.sh | sh -s -- -y > /dev/null 2>&1
+curl -fsSL https://starship.rs/install.sh | sh -s -- -y 
 echo "Installed starship"
 
 ## Additionnal softwares
 echo "Installing additional softwares..."
 
 # Install sccache first then the rest to speed up the process
-~/.cargo/bin/cargo install sccache > /dev/null 2>&1
+~/.cargo/bin/cargo install sccache 
 echo "Installed sccache"
 ## Array of cargo packages to install
 declare -a packages=(
@@ -64,11 +64,11 @@ declare -a packages=(
   "nu"
   "ripgrep"
 )
-RUSTC_WRAPPER=sccache ~/.cargo/bin/cargo install "${packages[@]}" > /dev/null 2>&1
+RUSTC_WRAPPER=sccache ~/.cargo/bin/cargo install "${packages[@]}" 
 echo "Installed" "${packages[@]}"
 
 ## Nvim
-~/.cargo/bin/bob use latest > /dev/null 2>&1
+~/.cargo/bin/bob use latest 
 echo "Installed nvim via bob"
 
 ## Brew if on MacOS
