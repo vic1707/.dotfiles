32d9ffcc052f2fe5638411c23a3141e90e2098ce
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-05-06 17:11:47 +0200

    feat: silence as many things as possible

--- scripts/softwares.sh
+++ scripts/softwares.sh
@@ -5,19 +5,21 @@ echo "Installing softwares..."
 
 ## Rust
 echo "Installing rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# quiet option doesn't seem to do much see https://github.com/rust-lang/rustup/issues/3350
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -q
 echo "Installed rust"
 
 ## Xmake
 echo "Installing xmake..."
-curl -fsSL https://xmake.io/shget.text | bash
+# cannot be quieter until https://github.com/xmake-io/xmake/issues/3714 is fixed
+curl -fsSL https://xmake.io/shget.text | bash 1>/dev/null
 echo "Installed xmake"
 
 ## Additionnal softwares
 echo "Installing additional softwares..."
 
 # Install sccache first then the rest to speed up the process
-~/.cargo/bin/cargo install sccache
+~/.cargo/bin/cargo install -q sccache
 echo "Installed sccache"
 ## Array of cargo packages to install
 declare -a packages=(
@@ -35,7 +37,7 @@ declare -a packages=(
   "rtx-cli"
   "starship"
 )
-RUSTC_WRAPPER=~/.cargo/bin/sccache ~/.cargo/bin/cargo install "${packages[@]}"
+RUSTC_WRAPPER=~/.cargo/bin/sccache ~/.cargo/bin/cargo install -q "${packages[@]}"
 echo "Installed" "${packages[@]}"
 
 ## Node
