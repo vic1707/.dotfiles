390f3cc587f29d029467b356243bd87fdc1ac225
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-05-10 11:11:46 +0200

    feat: rework base install script

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-ln -fs "$HOME/.dotfiles/shell/zsh/.zshrc" "$HOME/.zshrc"
-ln -fs "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
+################################
+##      check current dir     ##
+################################
+DOTS_DIR="$(cd "$(dirname "$0")" && pwd)"
+export DOTS_DIR
+# if DOTS_DIR is not `$HOME/.dotfiles`, warn and exit
+if [ "$DOTS_DIR" != "$HOME/.dotfiles" ]; then
+  echo "Error: DOTS_DIR is not $HOME/.dotfiles" >&2
+  exit 1;
+fi
+
+echo "Installing dotfiles"
+
+################################
+## create ln for config files ##
+################################
+ln -fs "$DOTS_DIR/.gitconfig" "$HOME/.gitconfig"
+ln -fs "$DOTS_DIR/shell/zsh/.zshrc" "$HOME/.zshrc"
+
 mkdir -p "$HOME/.config"
-ln -fs "$HOME/.dotfiles/.config/nvim" "$HOME/.config/nvim"
-ln -fs "$HOME/.dotfiles/.config/rtx" "$HOME/.config/rtx"
-ln -fs "$HOME/.dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
+if [ -n "$(ls -A "$HOME/.config")" ]; then
+  echo "Error: $HOME/.config already exists and is not empty" >&2
+  exit 1;
+fi
+ln -fs "$DOTS_DIR/.config"* "$HOME/.config"
 
 echo "Minimal config done"
 
@@ -13,8 +32,3 @@ echo "Minimal config done"
 FONT_DIR="$([ "$(uname)" = 'Darwin' ] && echo "$HOME/Library/Fonts" || echo "$HOME/.local/share/fonts")"
 cp -r "$HOME/.dotfiles/fonts/"* "$FONT_DIR"
 echo "Fonts installed"
-
-## install zsh plugins
-./scripts/zsh-plugins.sh
-## install software
-./scripts/softwares.sh
