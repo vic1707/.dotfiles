f85af723a5a99c53cd1b056fe1bcfc5215d20755
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-05-10 14:10:13 +0200

    feat: `mac_software` is now POSIX complient

---
+++ scripts/mac_softwares.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+###################################
+## This script needs to be POSIX ##
+###################################
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install brew packages
+PKGS="
+  ffmpeg
+  fzf
+  imagemagick
+  openssh
+  tmux
+  tree
+"
+# shellcheck disable=SC2046,SC2086
+brew install $PKGS # brew doesn't like quotes around `$PKGS`
+echo "Installing brew packages: $PKGS"
