57d768ac008b202fef787e59cb24871a06c54c02
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-05-10 12:03:37 +0200

    tmpfix: move `rtx` loading command since only zsh

--- shell/common/soft.sh
+++ shell/common/soft.sh
@@ -11,7 +11,3 @@ test -f "$HOME/.xmake/profile" && \. "$HOME/.xmake/profile" # load xmake profile
 # >>> Load cargo >>>
 test -f "$HOME/.cargo/env" && \. "$HOME/.cargo/env"
 # <<< Load cargo <<<
-
-# >>> Load RTX >>>
-eval "$(rtx activate zsh)"
-# <<< Load RTX <<<
