03afdc7f8dfd146140802ed14f5062eab0d6d7a2
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-06-07 13:59:30 +0200

    feat: install scripts can use helper functions

--- scripts/__index.sh
+++ scripts/__index.sh
@@ -4,6 +4,10 @@
 ## This script needs to be POSIX ##
 ###################################
 
+## source some helpers functions
+# shellcheck source=shell/common/functions.sh
+. "$DOTS_DIR/shell/common/functions.sh"
+
 # shellcheck source=scripts/config_files.sh
 . "$DOTS_DIR/scripts/config_files.sh"
 # shellcheck source=scripts/fonts.sh
