34e9bc2c54bba02cdff1d876f1bb059e3b47c82c
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-06-08 21:32:58 +0200

    feat: `softwares.sh` is now collection of functions

--- scripts/softwares.sh
+++ scripts/softwares.sh
@@ -4,28 +4,58 @@
 ## This script needs to be POSIX ##
 ###################################
 
-### Install softwares ###
-echo "Installing softwares..."
+install_softwares() {
+  (install_rust && echo "Rust installed") || {
+    echo "Failed to install rust"
+    return 1
+  }
+  (install_xmake && echo "xmake installed") || {
+    echo "Failed to install xmake"
+    return 1
+  }
+  (install_sccache && echo "sccache installed") || {
+    echo "Failed to install sccache"
+    echo "Cargo is not installed"
+    return 1
+  }
+  (install_cargo_pkgs && echo "Cargo packages installed") || {
+    echo "Failed to install cargo packages"
+    echo "Cargo is not installed"
+    return 1
+  }
+  (install_node && echo "Node installed") || {
+    echo "Failed to install rtx"
+    echo "Rtx is not installed"
+    return 1
+  }
+  (install_nvim && echo "Nvim installed") || {
+    echo "Failed to install nvim"
+    echo "Bob is not installed"
+    return 1
+  }
+}
 
-## Rust
-echo "Installing rust..."
-# quiet option doesn't seem to do much see https://github.com/rust-lang/rustup/issues/3350
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -q
-echo "Installed rust"
+install_rust() {
+  # quiet option doesn't seem to do much see https://github.com/rust-lang/rustup/issues/3350
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -q
+  # return error code of previous command
+  return $?
+}
 
-## Xmake
-echo "Installing xmake..."
-# cannot be quieter until https://github.com/xmake-io/xmake/issues/3714 is fixed
-curl -fsSL https://xmake.io/shget.text | bash 1>/dev/null
-echo "Installed xmake"
+install_xmake() {
+  # cannot be quieter until https://github.com/xmake-io/xmake/issues/3714 is fixed
+  curl -fsSL https://xmake.io/shget.text | bash 1>/dev/null
+  # return error code of previous command
+  return $?
+}
 
-## Additionnal softwares
-echo "Installing additional softwares..."
+install_sccache() {
+  [ -x ~/.cargo/bin/cargo ] && return 1
+  ~/.cargo/bin/cargo install -q sccache
+  # return error code of previous command
+  return $?
+}
 
-# Install sccache first then the rest to speed up the process
-~/.cargo/bin/cargo install -q sccache
-echo "Installed sccache"
-## Array of cargo packages to install
 CARGO_PKGS="
 bacon
 bat
@@ -47,16 +77,24 @@ ripgrep
 rtx-cli
 starship
 "
-# shellcheck disable=SC2046,SC2086
-RUSTC_WRAPPER=~/.cargo/bin/sccache ~/.cargo/bin/cargo install --locked -q $CARGO_PKGS # cargo doesn't like quotes around `$CARGO_PKGS`
-echo "Installed cargo softwares: $CARGO_PKGS"
+install_cargo_pkgs() {
+  [ -x ~/.cargo/bin/cargo ] || return 1
+  # shellcheck disable=SC2046,SC2086
+  RUSTC_WRAPPER=~/.cargo/bin/sccache ~/.cargo/bin/cargo install --locked -q $CARGO_PKGS # cargo doesn't like quotes around `$CARGO_PKGS`
+  # return error code of previous command
+  return $?
+}
 
-## Node
-~/.cargo/bin/rtx install node
+install_node() {
+  [ -x ~/.cargo/bin/rtx ] || return 1
+  ~/.cargo/bin/rtx install node
+  # return error code of previous command
+  return $?
+}
 
-## Nvim
-~/.cargo/bin/bob use latest
-echo "Installed nvim via bob"
-
-## Brew if on MacOS
-[ "$(uname -s)" = 'Darwin' ] && ./scripts/mac_softwares.sh
+install_nvim() {
+  [ -x ~/.cargo/bin/bob ] || return 1
+  ~/.cargo/bin/bob use latest
+  # return error code of previous command
+  return $?
+}
