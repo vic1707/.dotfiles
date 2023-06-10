67e894b8aaad220ec504495b4b0e8aa12a30afd3
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-06-10 11:56:10 +0200

    feat: fonts dir is now a symlink instead of a copy

--- scripts/2_fonts.sh
+++ scripts/2_fonts.sh
@@ -15,6 +15,6 @@
 #####################################
 install_fonts() {
   FONT_DIR="$([ "$(uname)" = 'Darwin' ] && echo "$HOME/Library/Fonts" || echo "$HOME/.local/share/fonts")"
-  cp -r "$HOME/.dotfiles/fonts/"* "$FONT_DIR"
+  ln -fs "$DOTS_DIR/fonts" "$FONT_DIR/custom_fonts"
   return 0
 }
