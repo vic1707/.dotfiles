e6ab351338b82a87808a7782d455dd68b26066f8
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-07-03 10:22:39 +0200

    feat: `bash` completions

--- shell/bash/completions.bash
+++ shell/bash/completions.bash
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+
+# RTX
+# shellcheck disable=SC1090 # sourced via external command
+. <(rtx activate bash)
+
+# Bob
+# shellcheck disable=SC1090 # sourced via external command
+. <(bob complete bash)
+
+# Cargo-flamegraph
+# shellcheck disable=SC1090 # sourced via external command
+. <(flamegraph --completions bash)
+
+# Carapace-bin
+# shellcheck disable=SC1090 # sourced via external command
+. <(carapace _carapace bash)
+
+# Starship
+# shellcheck disable=SC1090 # sourced via external command
+. <(starship completions bash)
