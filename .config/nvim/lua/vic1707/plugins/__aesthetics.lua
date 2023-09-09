25f808234ec4096e9da59c4eb75675c4fb965bf1
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-09-09 16:07:24 +0200

    breakdown plugins

---
+++ .config/nvim/lua/vic1707/plugins/__aesthetics.lua
@@ -0,0 +1,36 @@
+-- Aesthetics related configuration --
+return {
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme('onedark')
+    end,
+  },
+
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+  },
+}
