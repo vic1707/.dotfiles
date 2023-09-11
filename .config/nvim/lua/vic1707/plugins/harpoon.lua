d720d289c7b2aecf44af201bc3145da414cc09d0
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-09-11 11:57:51 +0200

    stylua

--- .config/nvim/lua/vic1707/plugins/harpoon.lua
+++ .config/nvim/lua/vic1707/plugins/harpoon.lua
@@ -6,13 +6,13 @@ return {
       ui = require('harpoon.ui')
       mark = require('harpoon.mark')
 
-      vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = '[H]arpoon [A]dd File' } )
-      vim.keymap.set('n', '<leader>hm', ui.toggle_quick_menu, { desc = '[H]arpoon [M]enu' } )
+      vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = '[H]arpoon [A]dd File' })
+      vim.keymap.set('n', '<leader>hm', ui.toggle_quick_menu, { desc = '[H]arpoon [M]enu' })
 
       for i = 1, 5 do
         vim.keymap.set('n', string.format('<leader>%s', i), function()
           ui.nav_file(i)
-        end, { desc = string.format('[]Harpoon Navigate to file [%s]', i) } )
+        end, { desc = string.format('[]Harpoon Navigate to file [%s]', i) })
       end
     end,
   },
