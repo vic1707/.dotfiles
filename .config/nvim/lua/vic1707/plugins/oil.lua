6fa99c7d41d6e102849b9b5e8acf93cee719292f
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-09-12 14:15:10 +0200

    oil and undotree

---
+++ .config/nvim/lua/vic1707/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  -- Buffer like file explorer
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('oil').setup()
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  end,
+}
