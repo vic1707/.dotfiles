094e74e019d12f277ba7ad19a41255d9d8aeacf0
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-09-12 15:22:35 +0200

    some more remaps

--- .config/nvim/lua/vic1707/remaps.lua
+++ .config/nvim/lua/vic1707/remaps.lua
@@ -1,8 +1,32 @@
 -- [[ Basic Keymaps ]]
 
+-- Disable command history `q:`
+vim.keymap.set('n', 'q:', '<Nop>', { silent = true })
+
 -- Keymaps for better default experience
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Move highlighted block of text in visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Keep cursor vertically centered while jumping half pages
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Keep cursor vertically centered while searching
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Keep buffer when pasting over
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- Replace current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
