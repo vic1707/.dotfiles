e0e206bde2841002e9fdc5f69b6df31f1357c561
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-09-12 15:21:51 +0200

    fix oil remap description

--- .config/nvim/lua/vic1707/plugins/oil.lua
+++ .config/nvim/lua/vic1707/plugins/oil.lua
@@ -4,6 +4,6 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('oil').setup()
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open current directory' })
   end,
 }
