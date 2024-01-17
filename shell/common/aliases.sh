6d9784477ea7fd99b55d67999e77acdfd0766e19
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-01-17 19:40:44 +0100

    fix aliases

--- shell/common/aliases.sh
+++ shell/common/aliases.sh
@@ -9,9 +9,7 @@ alias grep='grep --color'
 alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
 
 # cat
-if command -v bat > /dev/null; then
-  alias cat='bat'
-fi
+test -f ~/.cargo/bin/bat && alias cat='bat'
 
 # tail
 alias t='tail -f'
@@ -25,18 +23,15 @@ alias fd='find . -type d -name'
 alias ff='find . -type f -name'
 
 # ls
-if command -v exa > /dev/null; then
-  alias ls='exa --icons'
-fi
+test -f ~/.cargo/bin/exa && alias ls='exa --icons'
 alias l='ls -lF'
 alias la='ls -laF'
 alias ll='ls -la'
 
-# vi
-if command -v nvim > /dev/null; then
-  alias vi='nvim'
-  alias vim='nvim'
-fi
+# vi/vim
+NVIM_BIN="$HOME/.local/share/mise/installs/neovim/latest/bin/nvim"
+test -f "$NVIM_BIN" && alias vi="$NVIM_BIN"
+test -f "$NVIM_BIN" && alias vim="$NVIM_BIN"
 
 # docker
 if command -v docker > /dev/null; then
