60beb8b326108c1fbb74758b51c38ac5b0c3e431
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-01-18 19:03:22 +0100

    fix lint

--- .config/nvim/lua/vic1707/plugins/harpoon.lua
+++ .config/nvim/lua/vic1707/plugins/harpoon.lua
@@ -8,8 +8,12 @@ return {
       local harpoon = require('harpoon')
       local ui = require('harpoon.ui')
 
-      vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = '[H]arpoon [A]dd' })
-      vim.keymap.set('n', '<leader>hm', function() ui:toggle_quick_menu(harpoon:list()) end, { desc = '[H]arpoon [M]enu' })
+      vim.keymap.set('n', '<leader>ha', function()
+        harpoon:list():append()
+      end, { desc = '[H]arpoon [A]dd' })
+      vim.keymap.set('n', '<leader>hm', function()
+        ui:toggle_quick_menu(harpoon:list())
+      end, { desc = '[H]arpoon [M]enu' })
 
       for i = 1, 5 do
         vim.keymap.set('n', string.format('<leader>%s', i), function()
