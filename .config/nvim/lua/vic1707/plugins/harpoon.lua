907ea57cf20e6415ebcdbdce6d9033d838332004
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-01-18 18:42:24 +0100

    updae to harpoon2

--- .config/nvim/lua/vic1707/plugins/harpoon.lua
+++ .config/nvim/lua/vic1707/plugins/harpoon.lua
@@ -1,13 +1,15 @@
 return {
   {
     'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('harpoon').setup()
+      local harpoon = require('harpoon')
       local ui = require('harpoon.ui')
-      local mark = require('harpoon.mark')
 
-      vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = '[H]arpoon [A]dd File' })
-      vim.keymap.set('n', '<leader>hm', ui.toggle_quick_menu, { desc = '[H]arpoon [M]enu' })
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = '[H]arpoon [A]dd' })
+      vim.keymap.set('n', '<leader>hm', function() ui:toggle_quick_menu(harpoon:list()) end, { desc = '[H]arpoon [M]enu' })
 
       for i = 1, 5 do
         vim.keymap.set('n', string.format('<leader>%s', i), function()
