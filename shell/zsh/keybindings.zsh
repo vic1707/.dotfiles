4dae360f779408ddfdc054fa76c55ea523729f72
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-08-19 19:11:02 +0200

    add cmd+left/right bindings

--- shell/zsh/keybindings.zsh
+++ shell/zsh/keybindings.zsh
@@ -27,6 +27,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     key[Control_Left]="^[b"
     key[Control_Right]="^[f"
     key[Control_Backspace]="^H"
+    # CMD + Left/Right bindings
+    key[Cmd_Left]="^A"   # CMD + Left (start of line)
+    key[Cmd_Right]="^E"  # CMD + Right (end of line)
 fi
 
 # setup key accordingly
@@ -39,3 +42,7 @@ bindkey -- "${key[Shift_Tab]}" reverse-menu-complete
 bindkey -- "${key[Control_Left]}" backward-word
 bindkey -- "${key[Control_Right]}" forward-word
 bindkey -- "${key[Control_Backspace]}" backward-kill-word
+
+# CMD + Left/Right only if defined (so MacOs only)
+[[ -n "${key[Cmd_Left]}" ]] && bindkey -- "${key[Cmd_Left]}" beginning-of-line
+[[ -n "${key[Cmd_Right]}" ]] && bindkey -- "${key[Cmd_Right]}" end-of-line
